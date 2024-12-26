4e0a2e2252f4b03bb9ddebd238875e6f994432e0
mzencd <13688163567@139.com>
2024-12-26 16:54:53 +0800

    update test report

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    reporters: ['junit', 'html'],
+    outputFile: {
+        junit: './html/junit-report.xml'
+      }
+  }
+})
